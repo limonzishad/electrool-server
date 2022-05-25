33098f60d46d4da3a90db0d654b2acb9e0c44cdf
limonzishad <zishadlimon@gmail.com>
2022-05-26 00:53:12 +0600

    find admin completed

--- index.js
+++ index.js
@@ -98,6 +98,13 @@ async function run() {
             const result = await userCollection.updateOne(filter, updateDoc);
             res.send(result);
         });
+
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        });
     }
 
     finally {
